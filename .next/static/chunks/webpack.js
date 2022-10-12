9519169512598e264f316af0b321dc3fedfed3d1
zulmiross <zulejesus@hotmail.com>
2022-10-12 14:58:07 -0300

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5f8d886227bc1605"; }
+/******/ 		__webpack_require__.h = function() { return "7a60f8f53420c2d3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
