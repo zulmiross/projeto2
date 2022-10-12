79431cbeba19a4c047a01c759845dcf5b0191a34
zulmiross <zulejesus@hotmail.com>
2022-10-12 14:36:30 -0300

    alteração: index,js

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,7 @@ export default function Home() {
     <div>
       <h1>Projeto 2</h1>
       <p>{count}</p>
-      <button onClick={handleAddCount}>Adicionar +1</button>
-      <p>Adicionado agora</p>
+      <button onClick={handleAddCount}>Adicionar +1</button>      
     </div>
   );
 }
