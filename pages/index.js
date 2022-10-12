599747e2f160e56c20c4765f55b5eb93db3888e6
zulmiross <zulejesus@hotmail.com>
2022-10-12 14:24:21 -0300

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function Home() {
+  const [count, setCount] = useState(1);
+  function handleAddCount(){
+    setCount(count + 1);
+  }
+  return (
+    <div>
+      <h1>Projeto 2</h1>
+      <p>{count}</p>
+      <button onClick={handleAddCount}>Adicionar +1</button>
+    </div>
+  );
+}
